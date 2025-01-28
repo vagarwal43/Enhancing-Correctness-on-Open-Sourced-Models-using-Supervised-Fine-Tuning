98084dd95bc5d4ed30c7f755a138f4f2e645084a
Vasvi Agarwal <100332832+vagarwal43@users.noreply.github.com>
2025-01-27 22:49:52 -0500

    Delete Convert_to_HF directory

--- Convert_to_HF/prompts.py
+++
@@ -1,7 +0,0 @@
-initial_prompt = 'You are a math expert. When you respond, respond only with the Solution of the final Problem, thinking step by step. At the end of the Solution, when you give your final answer, write it in the form "Final Answer: The final answer is $answer$. I hope it is correct."'
-
-self_correcting_prompt = 'There might be an error in the solution above because of lack of understanding of the question. Please correct the error, if any, and rewrite the solution. Only output the final solution! At the end of the Solution, when you give your final answer, write it in the form "Final Answer: The final answer is $answer$. I hope it is correct."'
-
-MATH_initial_prompt = 'You are a math expert. When you respond, respond only with the Solution of the final Problem, thinking step by step. At the end of the Solution, when you give your final answer, write it in the form "Final Answer: \\boxed{$answer$}. I hope it is correct."'
-
-MATH_self_correcting_prompt = 'There might be an error in the solution above because of lack of understanding of the question. Please correct the error, if any, and rewrite the solution. Only output the final solution! At the end of the Solution, when you give your final answer, write it in the form "Final Answer: \\boxed{$answer$}. I hope it is correct."'
